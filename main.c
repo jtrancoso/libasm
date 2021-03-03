23a42ea2bbd1384355c34fa57df8590d9e7cf900
jtrancos <jesus.trancoso.jimenez@gmail.com>
2021-03-03 17:59:08 +0100

    limpieza

--- main.c
+++ main.c
@@ -4,25 +4,29 @@
 
 size_t	ft_strlen(char *);
 char	*ft_strcpy(char *dest, char *src);
+int		strcmp(const char *s1, const char *s2);
 
 int main (void)
 {
 	char dest[200];
 
-	printf("FT_STRLEN\n\n");
-	printf("strlen:    %zu\n", strlen("holaa"));
-	printf("ft_strlen: %zu\n", ft_strlen("holaa"));
+	printf("\033[4;32mFT_STRLEN\n\n\033[0m");
+	printf("string:    hola\n");
+	printf("strlen:    %zu\n", strlen("hola"));
+	printf("ft_strlen: %zu\n", ft_strlen("hola"));
 	printf("\n");	
 
+	printf("string:\n");
 	printf("strlen:    %zu\n", strlen(""));
 	printf("ft_strlen: %zu\n", ft_strlen(""));
 	printf("\n");
 
+	printf("string:    holawqudgweoudgvewyuvewcyewvcyiveiwcvwecvwcuewvcwVCWEYtcvwIYCTVewoucvweiycveOFGFYGFUGO8FGWEOUCvwcuwevcuwevcwegcvwycgvwecyvwcyewgvcwev\n");
 	printf("strlen:    %zu\n", strlen("holawqudgweoudgvewyuvewcyewvcyiveiwcvwecvwcuewvcwVCWEYtcvwIYCTVewoucvweiycveOFGFYGFUGO8FGWEOUCvwcuwevcuwevcwegcvwycgvwecyvwcyewgvcwev"));
 	printf("ft_strlen: %zu\n", ft_strlen("holawqudgweoudgvewyuvewcyewvcyiveiwcvwecvwcuewvcwVCWEYtcvwIYCTVewoucvweiycveOFGFYGFUGO8FGWEOUCvwcuwevcuwevcwegcvwycgvwecyvwcyewgvcwev"));
 	printf("\n");
 
-	printf("FT_STRCPY\n\n");
+	printf("\033[4;32mFT_STRCPY\n\n\033[0m");
 	printf("strcpy:    %s\n", strcpy(dest, "hola"));
 	printf("ft_strcpy: %s\n", ft_strcpy(dest, "hola"));
 	printf("\n");
@@ -30,7 +34,6 @@ int main (void)
 	printf("strcpy:    %s\n", strcpy(dest, ""));
 	printf("ft_strcpy: %s\n", ft_strcpy(dest, ""));
 	printf("\n");
-
 
 	printf("strcpy:    %s\n", strcpy(dest, "olawqudgweoudgvewyuvewcyewvcyiveiwcvwecvwcuewvcwVCWEYtcvwIYCTVewoucvweiycveOFGFYGFUGO8FGWEOUCvwcuwevcuwevcwegcvwycgvwecyvwcyewgvcwev"));
 	printf("ft_strcpy: %s\n", ft_strcpy(dest, "olawqudgweoudgvewyuvewcyewvcyiveiwcvwecvwcuewvcwVCWEYtcvwIYCTVewoucvweiycveOFGFYGFUGO8FGWEOUCvwcuwevcuwevcwegcvwycgvwecyvwcyewgvcwev"));
