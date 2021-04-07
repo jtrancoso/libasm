ae375ee6d13a63880e07d71abdc61d06472f68c5
jtrancos <jesus.trancoso.jimenez@gmail.com>
2021-04-07 15:42:11 +0200

    main 2.0

--- main.c
+++ main.c
@@ -13,90 +13,127 @@ ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
 
 void	test_strlen(char *s)
 {
+	int real;
+	int mine;
+
+	real = strlen(s);
+	mine = ft_strlen(s);
 	printf("string:    %s\n", s);
 	printf("strlen:    %zu\n", strlen(s));
 	printf("ft_strlen: %zu\n", ft_strlen(s));
-	printf("\n");
+	if (mine == real)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
 }
 
 void	test_strcpy(char *src)
 {
 	char dest[255];
 
+	char *real;
+	char *mine;
+
+	real = strcpy(dest, src);
+	mine = ft_strcpy(dest, src);
 	printf("src:       %s\n", src);
 	printf("strcpy:    %s\n", strcpy(dest, src));
 	printf("ft_strcpy: %s\n", ft_strcpy(dest, src));
-	printf("\n");
+	if (mine == real)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
 }
 
 void	test_strcmp(char *s1, char *s2)
 {
+	int	real;
+	int	mine;
+
+	real = strcmp(s1, s2);
+	mine = ft_strcmp(s1, s2);
 	printf("s1: %s s2: %s\n", s1, s2);
 	printf("strcmp:    %d\n", strcmp(s1, s2));
 	printf("ft_strcmp: %d\n", ft_strcmp(s1, s2));
-	printf("\n");
+	if (mine == real)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
 }
 
 void	test_strdup(char *src)
 {
-	char *aux;
-	char *aux2;
+	char *real;
+	char *mine;
+	int true_errno;
+	int my_errno;
 
 	printf("string:          %s\n", src);
-	aux = strdup(src);
-	printf("strdup:          %s\n", aux);
+	real = strdup(src);
+	true_errno = errno;
+	printf("strdup:          %s\n", real);
+	printf("errno:           %d\n", true_errno);
 	perror(0);
 
-	aux2 = ft_strdup(src);
-	printf("ft_strdup:       %s\n", aux2);
+	mine = ft_strdup(src);
+	my_errno = errno;
+	printf("ft_strdup:       %s\n", mine);
+	printf("my_errno:        %d\n", my_errno);
 	perror(0);
+	if (ft_strcmp(real, mine) == 0 && true_errno == my_errno)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
+	if (real)
+		free(real);
+	if (mine)
+		free(mine);
+	errno = 0;
 
-	if (aux)
-		free(aux);
-	if (aux2)
-		free(aux2);
-	printf("\n");
 }
 
 void	test_write(int fd, char *str)
 {
 	int true_errno;
 	int my_errno;
-	int original;
+	int real;
 	int original_errno;
-	int mine;
-	int mine_errno;
+	ssize_t mine;
+	ssize_t mine_errno;
+
 
 	if (strchr(str, '\n'))
 	{
 		write(1, "\033[0mreal write: ", 16);
-		write(fd, str, ft_strlen(str));
+		real = write(fd, str, ft_strlen(str));
 		true_errno = errno;
 		printf("real errno: %d\n", true_errno);
 		ft_write(1, "ft_write:   ", 12);
-		ft_write(fd, str, ft_strlen(str));
+		mine = ft_write(fd, str, ft_strlen(str));
 		my_errno = errno;
 		printf("my errno:   %d\n", my_errno);
 		perror(0);
-		write(1, "\n", 1);
 		errno = 0;
 	}
 	else
 	{
 		write(1, "\033[0mreal write: ", 16);
-		write(fd, str, ft_strlen(str));
+		real = write(fd, str, ft_strlen(str));
 		true_errno = errno;
 		write(1, "\n", 1);
 		printf("real errno: %d\n", true_errno);
 		ft_write(1, "ft_write:   ", 12);
-		ft_write(fd, str, ft_strlen(str));
+		mine = ft_write(fd, str, ft_strlen(str));
 		my_errno = errno;
 		write(1, "\n", 1);
 		printf("my errno:   %d\n", my_errno);
 		perror(0);
-		write(1, "\n", 1);
 		errno = 0;
 	}
+	if (mine == real && true_errno == my_errno)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
 
 }
 
@@ -106,21 +143,26 @@ void	test_read(int fd)
 	int	true_errno;
 	int	my_errno;
 	int i;
+	ssize_t real;
+	ssize_t mine;
 
 	write(1, "\033[0mInsert text to read:\n> ", 27);
-	read(fd, buffer, 100);
+	real = read(fd, buffer, 100);
 	true_errno = errno;
 	printf("\nOriginal: %s", buffer);
 	printf("errno(%d)\n", true_errno);
 	perror(0);
 	write(1, "\n", 1);
 	write(1, "Insert text to ft_read:\n> ", 26);
-	ft_read(fd, buffer, 100);
+	mine = ft_read(fd, buffer, 100);
 	my_errno = errno;
 	printf("\nMy read: %s", buffer);
 	printf("errno(%d)\n", my_errno);
 	perror(0);
-	write(1, "\n", 1);
+	if (mine == real && true_errno == my_errno)
+		printf("\033[1;32m[OK]\n\n\033[0m");
+	else
+		printf("\033[1;31m[KO]\n\n\033[0m");
 	errno = 0;
 }
 
@@ -130,7 +172,7 @@ int main (void)
 	char *aux;
 	char *aux2;
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_STRLEN ========\n");
 	printf("=============================\n\n\033[0m");
 
@@ -138,7 +180,7 @@ int main (void)
 	test_strlen("");
 	test_strlen("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_STRCPY ========\n");
 	printf("=============================\n\n\033[0m");
 
@@ -147,7 +189,7 @@ int main (void)
 	test_strcpy("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_STRCMP ========\n");
 	printf("=============================\n\n\033[0m");
 
@@ -156,7 +198,7 @@ int main (void)
 	test_strcmp("h", "hh");
 	test_strcmp("", "");
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_STRDUP ========\n");
 	printf("=============================\n\n\033[0m");
 
@@ -164,7 +206,7 @@ int main (void)
 	test_strdup("");
 	test_strdup("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_WRITE =========\n");
 	printf("=============================\n\n\033[0m");
 
@@ -175,7 +217,7 @@ int main (void)
 	test_write(2, "\0\0\0");
 	test_write(2, " ");
 
-	printf("\033[1;32m\n=============================\n");
+	printf("\033[1;34m\n=============================\n");
 	printf("========== FT_READ ==========\n");
 	printf("=============================\n\n\033[0m");
 
